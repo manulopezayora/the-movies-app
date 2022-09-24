4b0d291d4c6f4ae7bdf504f0322bbf750df2a042
Manu <manulopezauora1985@protonmail.com>
2022-09-25 00:15:48 +0200

    [DEV] Add register form

---
+++ src/app/user/models/Register.model.ts
@@ -0,0 +1,7 @@
+export type Register = {
+    username: string;
+    password: string;
+    password2: string;
+    email: string;
+    favMovies: any[];
+}
--- src/app/user/pages/register/register.component.ts
+++ src/app/user/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Register } from '../../models/Register.model';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public registerForm!: FormGroup;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
+  }
+
+  public sendRegisterForm(): void {
+    if (this.registerForm.valid) {
+      this.sendRegister(this.registerForm.value);
+    }
+  }
+
+  private sendRegister(user: Register): void {
+    this.userService.sendLogin(user);
+  }
+
+  private createRegisterForm(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      password2: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email])
+    })
   }
 
 }
