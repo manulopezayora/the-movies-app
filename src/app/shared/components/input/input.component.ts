d4a658f7b03be7a62c5b8771f43bca8b170a64ef
Manu <manulopezauora1985@protonmail.com>
2022-09-24 23:48:52 +0200

    [FIX] input value to formControl use

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
@@ -8,19 +8,57 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
-      useExisting: InputComponent,
-      multi: true
-    }]
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
 export class InputComponent implements OnInit {
   @Input() label: string | undefined;
   @Input() inputType: string | undefined;
 
+  public inputValue: string;
+  public value: string;
+  public isDisabled: boolean;
+
+  onChange = (_: any) => {};
+  onTouch = () => {};
+
   constructor() {
+    this.inputValue = '';
+    this.value = '';
+    this.isDisabled = false;
     this.inputType = this.inputType || 'text';
   }
 
   ngOnInit(): void {
+  }
+
+  public onInput(event: any): void {
+    this.inputValue = event.target.value;
+    this.value = this.inputValue;
+    this.onTouch();
+    this.onChange(this.value);
+  }
+
+  public writeValue(value: any): void {
+    if (value) {
+      this.value = value || '';
+    } else {
+      this.value = '';
+    }
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
   }
 
 }
