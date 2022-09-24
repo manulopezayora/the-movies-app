33dad7f76d790942f1e5d3d8e64258b93cf5a9e0
Manuel Jesús López <51477144+manulopezayora@users.noreply.github.com>
2022-09-25 01:52:03 +0200

    [DEV] Add new pages

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { MoviesListComponent } from './pages/movies-list/movies-list.component';
+import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MoviesListComponent,
+    MovieDetailComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
